774808b14f4d6e665f1288c42dc60f8c872f92c6
AlekPet <alexepetrof@gmail.com>
2023-11-30 21:06:22 +0300

    Add files prompt_translate and readme.

---
+++ prompt_translate/README.md
@@ -0,0 +1,10 @@
+# Prompt tranlsate
+
+![Prompt tranlsate image ](https://raw.githubusercontent.com/AlekPet/Fooocus_Extensions_AlekPet/main/assets/images/image_prompt_translate.jpg)
+
+### Installation:
+
+1. Backup one file in the Fooocus, **webui.py** in the root Fooocus direcory.
+2. Download files as zip file
+3. Extract the data from the archive and copy or move it from the **prompt_translate** folder to, where install **Fooocus** , and confirm replace all.
+4. Run Fooocus.
---
+++ prompt_translate/modules/module_translate.py
@@ -0,0 +1,34 @@
+from modules.launch_util import is_installed, run_pip
+
+packageName = "deep-translator"
+
+if is_installed(packageName):
+    run_pip("uninstall {packageName}", f"Prompt_translate uninstall -> {packageName}")
+
+if not is_installed(packageName):
+    run_pip(f"install {packageName}", f"Prompt_translate requirement -> {packageName}")
+
+from deep_translator import GoogleTranslator
+
+def translate(prompt, prompt_neg='', srcTrans="auto", toTrans="en"):
+    if not srcTrans:
+        srcTrans = 'auto'
+        
+    if not toTrans:
+        toTrans = 'en'
+        
+    print(f"{GREEN}-{prompt}={prompt_neg}={srcTrans}--->{toTrans}{CLEAR}")
+
+    tranlate_text_prompt = ''
+    if prompt and prompt.strip()!="":
+        tranlate_text_prompt = GoogleTranslator(source=srcTrans, target=toTrans).translate(prompt) 
+
+    tranlate_text_prompt_neg = ''
+    if prompt_neg and prompt_neg.strip()!="":
+        tranlate_text_prompt_neg = GoogleTranslator(source=srcTrans, target=toTrans).translate(prompt_neg) 
+
+    return [tranlate_text_prompt, tranlate_text_prompt_neg]
+
+GREEN = '\033[92m'
+CLEAR = '\033[0m'
+print(f"{GREEN}Prompt translate module AlekPet -> Loaded{CLEAR}")
